a1d5c13f68878dc815c77a1b0a92cff2c7c5f867
AssMasster <ivankozelskij@gmail.com>
2026-02-24 01:31:33 +0300

    Added gighlighting of matching sections for the search bar and task

--- src/entites/todo/ui/TodoItem/TodoItem.jsx
+++ src/entites/todo/ui/TodoItem/TodoItem.jsx
@@ -2,6 +2,7 @@ import { memo, useContext } from "react"
 import { TasksContext } from "@/entites/todo"
 import RouterLink from "@/shared/ui/RouterLink"
 import styles from './TodoItem.module.scss'
+import { highlightCaseInsensitive } from "@/shared/utils/highlight"
 const TodoItem = (props) => {
     const {
         id,
@@ -15,8 +16,11 @@ const TodoItem = (props) => {
         firstIncompleteTaskId,
         deleteTask,
         toggleTaskComplete,
-        disappearingTaskId
+        disappearingTaskId,
+        searchQuery,
     } = useContext(TasksContext)
+
+    const highlightedTitle = highlightCaseInsensitive(title, searchQuery)
 
     return (
         <li ref={id === firstIncompleteTaskId ? firstIncompleteTaskRef : null} className={`${styles.item} ${className} ${disappearingTaskId === id ? styles.isDisappearing : ''}`}>
@@ -34,7 +38,7 @@ const TodoItem = (props) => {
             {title}
           </label>
           <RouterLink to={`/tasks/${id}`} aria-label="Task detail page">
-            {title}
+            <span dangerouslySetInnerHTML={{__html: highlightedTitle}}/>
           </RouterLink>
           <button
             className={styles.deleteButton}
